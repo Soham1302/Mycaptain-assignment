de31bc635991cdee7066b2aa16e7e59f8dcaaba6
Soham1302 <80255611+Soham1302@users.noreply.github.com>
2021-03-08 16:17:07 +0530

    Add files via upload

---
+++ assignment part1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	char charType;
+	int integerType;
+	float floatType;
+	double doubleType;
+	
+	cout<<"Size of char:"<<sizeof(charType)<<" bytes"<<"\n";
+	cout<<"Size of int:"<<sizeof(integerType)<<" bytes"<<"\n";
+	cout<<"Size of float:"<<sizeof(floatType)<<" bytes"<<"\n";
+	cout<<"Size of double:"<<sizeof(doubleType)<<" bytes"<<"\n";
+	
+	return 0;
+}
---
+++ assignment part2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float number1 = 3.4;
+	float number2 = 5.5;
+	
+	cout<<"Enter two numbers: "<<number1<<"\n";
+	cout<<number2<<"\n";
+	
+	float product = number1 * number2;
+	
+	cout<<"Product = "<<product<<"\n";
+	
+	return 0;
+}
